172f9f291a3e8052998e5d35017824bc3b044dc3
TrungNguyen <trungcule1803@gmail.com>
2023-08-08 22:22:37 +0700

    upd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
     appAPI: 'http://14.225.2.63:8088/',
+    // appAPI: 'http://localhost:8088/',
     hrmsIMGPath: 'hrmsimg',
     hrmsPath: 'hrms',
     apifilePath: 'apifile',
